b645cb859cedaa92ad192b54d6b1a19ea5498f17
Robert Cireasa <cireasarobert@yahoo.com>
2019-11-03 16:37:32 +0000

    Add coordinates, format instructions

---
+++ SurveyShips/SurveyShips/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SurveyShips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> shipsInfo = new List<string>();
+            string gridSize, shipInfo;
+
+            //Get user input
+            Console.WriteLine("Please enter the dimensions for the grid");
+            gridSize = Console.ReadLine();
+            Console.WriteLine("Please enter coordinates and instructions for all ships on separate lines.");
+            shipInfo = Console.ReadLine();
+            while (!shipInfo.Equals(""))
+            {
+                shipsInfo.Add(shipInfo);
+                shipInfo = Console.ReadLine();
+            }
+
+            //Format user input
+            int.TryParse(gridSize.Split(" ")[0], out int dimensionX);
+            int.TryParse(gridSize.Split(" ")[1], out int dimensionY);
+        }
+    }
+}
