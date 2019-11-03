2071076a5d05914e532ce97bf249a1a033c9755a
Robert Cireasa <cireasarobert@yahoo.com>
2019-11-03 18:02:40 +0000

    Add logic to move ships on the grid

--- SurveyShips/SurveyShips/Program.cs
+++ SurveyShips/SurveyShips/Program.cs
@@ -26,22 +26,86 @@ namespace SurveyShips
             int.TryParse(gridSize.Split(" ")[1], out int dimensionY);
 
             //Array with cardinal points
-            string[] cardPoints = {"N", "W", "S", "E"};
+            string[] cardPoints = {"N", "E", "S", "W"};
 
             //Array to store location for each ship
             string[] shipLoc;
 
-            string shipCoords, instructions;
+            string shipCoords, instrs;
+            int i = 0;
 
             //Move ships
-            for(int i=0; i<ships.Count; i += 2)
+            for(int j=0; j<ships.Count; j += 2)
             {
-                shipCoords = ships[i];
-                instructions = ships[i + 1];
+                shipCoords = ships[j];
+                instrs = ships[j + 1];
                 shipLoc = shipCoords.Split(" ");
+
+                //Format ship location
                 int.TryParse(shipLoc[0], out int shipX);
                 int.TryParse(shipLoc[1], out int shipY);
-                int.TryParse(shipLoc[2], out int shipOrientation);
+
+                //Set i to current orientation of ship
+                switch (shipLoc[2])
+                {
+                    case "N":
+                        i = 0;
+                        break;
+                    case "E":
+                        i = 1;
+                        break;
+                    case "S":
+                        i = 2;
+                        break;
+                    case "W":
+                        i = 3;
+                        break;
+                }
+
+                foreach(char instr in instrs)
+                {
+                    if (instr == 'F')
+                    {
+                        if (i == 0)
+                        {
+                            shipY += 1;
+                        }
+                        else if(i == 1)
+                        {
+                            shipX += 1;
+                        }
+                        else if (i == 2)
+                        {
+                            shipY -= 1;
+                        }
+                        else if (i == 3)
+                        {
+                            shipX -= 1;
+                        }
+                    }
+                    else if(instr == 'L')
+                    {
+                        if(i - 1 < 0)
+                        {
+                            i = 3;
+                        }
+                        else
+                        {
+                            i -= 1;
+                        }
+                    }
+                    else if (instr == 'R')
+                    {
+                        if (i + 1 > 3)
+                        {
+                            i = 0;
+                        }
+                        else
+                        {
+                            i += 1;
+                        }
+                    }
+                }
             }
         }
     }
